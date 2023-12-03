8a4650fc3311b28dd93adeb296af31ae2e768e2e
Leerlandais <leerlandais@gmail.com>
2023-12-03 10:41:28 +0100

    added colour change on click

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("logicTable").addEventListener("click", function(event) {
         if (event.target.classList.contains("logButton")) {
             console.log("Clicked square:", event.target.id);
+            var sqClicked = event.target;
         }
+        if (sqClicked.style.backgroundColor ===""){
+            sqClicked.style.backgroundColor = "red";
+            }else if(sqClicked.style.backgroundColor === "red"){
+                sqClicked.style.backgroundColor = "green";            
+            }else if(sqClicked.style.backgroundColor === "green"){
+                sqClicked.style.backgroundColor = "";
+            }
     });
 });
 
